65a8ef66148e513b7649a340b51d6a68e19f8358
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2024-01-24 16:14:19 +0100

    Update teams.spec.ts

--- tests/teams.spec.ts
+++ tests/teams.spec.ts
@@ -3,7 +3,11 @@ import {expect, test} from "@playwright/test";
 test ('add new team', async ({ page }) => {
     await page.goto('https://l.hr.dmerej.info/add_team');
 
-    await page.fill('//*[@id="id_name"]', 'Team 1');
+    function randomName() {
+        return Math.random().toString(36).substring(7);
+    }
+
+    await page.fill('//*[@id="id_name"]', randomName());
     await page.getByRole('button', { name: 'Add' }).click();
 
     await expect(page).toHaveURL('https://l.hr.dmerej.info/teams');
