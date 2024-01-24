fee1f40d3defc2d0ea44a62432a0c9bb94f38541
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2024-01-24 16:51:43 +0100

    feat(employee): add Hiring bug test

--- tests/employee.spec.ts
+++ tests/employee.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { createEmployee } from "../libs/createEmployee";
-import { resetDatabase } from "../libs/resetDatabase.ts";
+import { resetDatabase } from "../libs/resetDatabase";
 
 test("add new employee", async ({ page }) => {
   await createEmployee(page);
@@ -25,4 +25,20 @@ test("zip code employee should be positive", async ({ page }) => {
   await page.goto("https://l.hr.dmerej.info/employees");
   await expect(page.getByText("Tom Scott")).not.toBeVisible();
   await resetDatabase(page);
+});
+
+
+test("edit hiring date", async ({ page }) => {
+  await resetDatabase(page);
+  await createEmployee(page);
+  await page.goto('https://l.hr.dmerej.info/employees');
+
+  const editLinkSelector = 'a.btn-primary';
+  const href = await page.$eval(editLinkSelector, (link) => link.getAttribute('href'));
+  await page.goto(`https://l.hr.dmerej.info${href}`);
+  await page.goto(`https://l.hr.dmerej.info${href}/contract`);
+
+  await page.fill('//*[@id="id_hiring_date"]', "2021-06-02");
+
+  await expect(page).toHaveURL(`https://l.hr.dmerej.info${href}`);
 });
