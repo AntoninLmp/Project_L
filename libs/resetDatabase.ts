d53e30d503aa758b85b9d3da7abfca43a484a26d
Sawangg <38500427+Sawangg@users.noreply.github.com>
2024-01-24 16:26:12 +0100

    feat(libs): added database reset

---
+++ libs/resetDatabase.ts
@@ -0,0 +1,6 @@
+import type { Page } from "@playwright/test";
+
+export const resetDatabase = async (page: Page) => { 
+  await page.goto("https://l.hr.dmerej.info/reset_db");
+  await page.click('button[type="submit"]');
+};
--- tests/employee.spec.ts
+++ tests/employee.spec.ts
@@ -1,7 +1,9 @@
 import { test, expect } from "@playwright/test";
 import { createEmployee } from "../libs/createEmployee";
+import { resetDatabase } from "../libs/resetDatabase.ts";
 
 test("add new employee", async ({ page }) => {
   await createEmployee(page);
   await expect(page).toHaveURL("https://l.hr.dmerej.info/employees");
+  await resetDatabase(page);
 });
