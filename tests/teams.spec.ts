3eaa588ad5e3119dc37b576a08deba9393105ea8
egsx <enzo.cornacch@gmail.com>
2024-01-24 16:40:20 +0100

    feat(teams): add  existing teams

--- tests/teams.spec.ts
+++ tests/teams.spec.ts
@@ -1,4 +1,5 @@
 import {expect, test} from "@playwright/test";
+import { resetDatabase } from "../libs/resetDatabase";
 
 test ('add new team', async ({ page }) => {
     await page.goto('https://l.hr.dmerej.info/add_team');
@@ -11,4 +12,24 @@ test ('add new team', async ({ page }) => {
     await page.getByRole('button', { name: 'Add' }).click();
 
     await expect(page).toHaveURL('https://l.hr.dmerej.info/teams');
+});
+
+test ('add existing team', async ({ page }) => {
+    await resetDatabase(page);
+
+    await page.goto('https://l.hr.dmerej.info/add_team');
+
+    await page.fill('//*[@id="id_name"]', 'Team 1');
+    await page.getByRole('button', { name: 'Add' }).click();
+
+    await expect(page).toHaveURL('https://l.hr.dmerej.info/teams');
+
+    await page.goto('https://l.hr.dmerej.info/add_team');
+
+    await page.fill('//*[@id="id_name"]', 'Team 1');
+    await page.getByRole('button', { name: 'Add' }).click();
+
+    await expect(page).toHaveURL('https://l.hr.dmerej.info/add_team');
+
+    //void resetDatabase(page);
 });
